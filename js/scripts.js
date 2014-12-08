55082ece7d4431238ddef3c055b000c85d369e3c
kmajones <kmajones@eagles.ewu.edu>
2014-12-08 09:53:19 -0800

    Basic information

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
 var cheney = "99004";
 var spokane = "99201";
+var ninemilefalls = "99201";
 
 $.simpleWeather({
     
-    location: 'spokane',
+    location: 'cheney',
     woeid: '',
     unit: 'f',
     
     success: function(weather) {
+
+     $('weathericon').text('<i class="icon-'+weather.code+'"></i>');   
      $('.temp').text(weather.temp);
      $('.current').text(weather.currently);  
+     $('.image').html('<i class="icon-'+weather.code+'"></i> I am testing this');
      $('.header').text(weather.city);  
     },
     
@@ -17,60 +21,3 @@ $.simpleWeather({
       $("#weather").html('<p>'+error+'</p>');
     }
   });
-
-//Geolocation 
-
-// REF: http://foundation.zurb.com/docs/
-// REF: http://simpleweatherjs.com/
-
-// 1. Check for Geolocation
-// REF: http://codepen.io/fleeting/pen/Idsaj
-// Browser support geolocation?  
-  if (navigator.geolocation) {
-    // Yes! Show button
-    $('.getGeolocation').show(); 
-  } else {
-    // No. Hide button
-    $('.getGeolocation').hide();
-  }
-
-// 2. Get Geolocation & return Simple Weather
-$('.getGeolocation').on('click', function() {
-  
-    navigator.geolocation.getCurrentPosition(function(position) {
-    //load weather using your lat/lng coordinates. See _loadWeather()_ below
-    loadWeather(position.coords.latitude+','+position.coords.longitude); 
-    // See latitute & longitude. Note, wait a few seconds
-    console.log(position.coords.latitude+','+position.coords.longitude);
-  });
-  
-});
-
-// 3. Wrap SimpleWeather in a function called _loadWeather()
-var loadWeather = function(location) {
-    
-    $.simpleWeather({
-    location: location,
-    
-    // Get _weather_ object
-    success: function(weather) {
-      
-      // Get & store temperature
-      var temp = weather.temp;
-      // Get & store city
-      var city = weather.city;
-      
-      // Output to hooks in HTML
-      $('.temp').text(temp);
-      $('.city').text(city);
-      
-      // See console for _weather_ object
-      console.log(weather);
-    }
-  
-  });
-    
-}; // end of _loadWeather()_ function
-
-
-
