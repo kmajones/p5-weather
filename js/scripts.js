4b188fa2ff16bc463b620c6412b197b1a41cba4e
kmajones <designer2@EWU-Rename-6.local>
2014-12-08 16:58:09 -0800

    added second part

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,53 @@
-var cheney = "99004";
-var spokane = "99201";
-var ninemilefalls = "99201";
+
 
 $.simpleWeather({
+     zipcode:'',
+    location: '',
+    woeid: '2378872',
+    unit: 'f',
     
-    location: 'cheney',
+    success: function(weather) {
+
+     $('weathericon').text('<i class="icon-'+weather.code+'"></i>');  
+     $('.city').text(weather.city); 
+     $('.temp').text(weather.temp);
+     $('.current').text(weather.currently);  
+     $('.image').html('<img src=' +weather.image +'>');
+     $('.header').text(weather.city);  
+     $('.high').text(weather.high);
+     $('.low').text(weather.low);
+    
+    $("#weather1").html(html);
+    },
+    
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+
+// Spokane
+
+$.simpleWeather({
+    zipcode:'',
+    location: '2460029',
     woeid: '',
     unit: 'f',
     
     success: function(weather) {
 
-     $('weathericon').text('<i class="icon-'+weather.code+'"></i>');   
+     $('weathericon').text('<i class="icon-'+weather.code+'"></i>');  
+     $('.city').text(weather.city); 
      $('.temp').text(weather.temp);
      $('.current').text(weather.currently);  
-     $('.image').html('<i class="icon-'+weather.code+'"></i> I am testing this');
+     $('.image').html('<img src=' +weather.image +'>');
      $('.header').text(weather.city);  
+     $('.high').text(weather.high);
+     $('.low').text(weather.low);
+     
+     $("#weather2").html();
     },
     
     error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
+      $("#weather2").html('<p>'+error+'</p>');
     }
   });
