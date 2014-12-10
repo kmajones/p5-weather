41bdc7b73811e807b7329824ffaec7998e0ceab1
kmajones <kmajones@eagles.ewu.edu>
2014-12-10 09:04:32 -0800

    Working Geo location and enter zip to one ID

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,6 @@
 
 $(document).ready(function() {
-  $.simpleWeather({
-    zipcode: '99026',
-    woeid: '12799444', //2357536
-    location: '',
-    unit: 'f',
-    success: function(weather) {
-     $('#weather1 .temperature').text(weather.temp+weather.units.temp);
-     //$(' #weather1 .weathericon').text('<i class="icon-'+weather.code+'"></i>');  
-     $('#weather1 .city').text(weather.city+', '+weather.region); 
-     $('#weather1 .temp').text(weather.temp);
-     $('#weather1 .current').text(weather.currently);  
-     $('#weather1 .image').html('<img src=' +weather.image +'>');
-     $('#weather1 .header').text(weather.city);  
-     $('#weather1 .high').text(weather.high);
-     $('#weather1 .low').text(weather.low);
 
-     $('#weather1 .day1').text(weather.forecast[1].high);
-     $('#weather1 .day2').text(weather.forecast[2].high);
-     
-      //html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-     
-      $("#weather1").html();
-    },
-    error: function(error) {
-      $("#weather1").html('<p>'+error+'</p>');
-    }
-  });
 
 //geolocation
 // REF: http://foundation.zurb.com/docs/
@@ -66,13 +40,13 @@ var loadWeather = function(location) {
       
       
       // Output to hooks in HTML
-      $('#weather2 .temp').text(weather.temp);
-      $('#weather2 .city').text(weather.city+', '+weather.region);
-      $('#weather2 .current').text(weather.currently);  
-      $('#weather2 .image').html('<img src=' +weather.image +'>');
-      $('#weather2 .header').text(weather.city);  
-      $('#weather2 .high').text(weather.high);
-      $('#weather2 .low').text(weather.low);
+      $(' .temp').text(weather.temp);
+      $(' .city').text(weather.city+', '+weather.region);
+      $(' .current').text(weather.currently);  
+      $(' .image').html('<img src=' +weather.image +'>');
+      $(' .header').text(weather.city);  
+      $(' .high').text(weather.high+'/');
+      $(' .low').text(weather.low);
       
       // See console for _weather_ object
       console.log(weather);
@@ -83,10 +57,50 @@ var loadWeather = function(location) {
 }; // end of _loadWeather()_ function
   
 
-  //accordion
-  $('.ui.accordion')
-  .accordion()
-;
+
+//Enter Zip
+
+// REF: http://foundation.zurb.com/docs/
+// REF: http://simpleweatherjs.com/
+
+// On click button, get zip, then run Simple Weather
+$('button').on('click', function() {
+  
+  // 1. Get & store entered zipcode
+  var zipcode = $('#getWeather').val();
+  
+  // 2. Pass weather into _simpleWeather()_ object
+  $.simpleWeather({
+    
+    location: zipcode,
+  
+    success: function(weather) {
+      
+      
+      // Output to hooks in HTML
+      $('.temp').text(weather.temp);
+      $('.city').text(weather.city+', '+weather.region);
+      $('.current').text(weather.currently);  
+      $('.image').html('<img src=' +weather.image +'>');
+      $('.header').text(weather.city);  
+      $('.high').text(weather.high +'/');
+      $('.low').text(weather.low);
+
+      // See console for all properties of object
+      console.log(weather);
+    },
+  
+    error: function(error) {
+      $('body').html('<p>' + error + '</p>');
+    }
+  
+  });
+  
+  // 3. Reset input value
+  $('#getWeather').val('');
+  
+});
+
 
 
 
